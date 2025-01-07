95f517d16d80121c4ea694283a113452dd0dada7
AparnaGhorpade <aparnag.7276@gmail.com>
2025-01-07 06:49:25 +0530

    Changes

---
+++ src/ex_01_Java_Basics/Lab002_ShortCutHW.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class Lab002_ShortCutHW {
+    public static void main(String[] args) {
+        System.out.println("Shortcuts of main and println method");
+    }
+}
