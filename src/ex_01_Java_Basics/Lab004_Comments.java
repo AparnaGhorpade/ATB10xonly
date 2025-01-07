7d880ea9bf743834f143649e101ef6569aa63327
AparnaGhorpade <aparnag.7276@gmail.com>
2025-01-07 06:59:42 +0530

    Changes

--- src/ex_01_Java_Basics/Lab004_Comments.java
+++ src/ex_01_Java_Basics/Lab004_Comments.java
@@ -1,4 +1,14 @@
 package ex_01_Java_Basics;
 
 public class Lab004_Comments {
+    public static void main(String[] args) {
+        //Single comments : Below code will print "To print"
+        System.out.println("To print");
+        /* Multiline comments
+        This is the multi line comments
+        .
+        .
+        .
+         */
+    }
 }
